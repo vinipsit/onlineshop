0b53dce5cd8579e1fb9cb7a776211c8de158e490
vinita <vineeta.vini1@gmail.com>
2015-01-09 17:38:43 +0700

    9jan

---
+++ app/controllers/reseller_controller.rb
@@ -0,0 +1,2 @@
+class ResellerController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onlineshop::Application.config.secret_key_base = '00fdf3556942cee5982d0dd7e1c474efff81095f1eb7aa89c5cac8c1b0422e2a331aa4fe982b99d1cc4c12c1e18fe179c12b1b42a85ff15d2d161a6367f746cb'
